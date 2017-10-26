bf2b30736b68e57a5b48e92159d86ef36a8ffff0
Elyas Dridba <elyas.dridba@gmail.com>
2017-10-26 11:39:22 +0200

    Add ReactRenderer Dependency + configuration + twig extension

--- DependencyInjection/EngineExtension.php
+++ DependencyInjection/EngineExtension.php
@@ -15,19 +15,21 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class EngineExtension extends Extension implements PrependExtensionInterface
 {
+    private $config;
+
     /**
      * {@inheritdoc}
      */
     public function prepend(ContainerBuilder $container)
     {
         $configs = $container->getExtensionConfig($this->getAlias());
-        $config = $this->processConfiguration(new Configuration(), $configs);
+        $this->config = $this->processConfiguration(new Configuration(), $configs);
         $kernelRootDir = $container->getParameter('kernel.root_dir');
 
-        $rootDir = $container->getParameterBag()->resolveValue($config['root_dir']);
+        $rootDir = $container->getParameterBag()->resolveValue($this->config['root_dir']);
 
         if (false === realpath($rootDir)) {
-            throw new \Exception(sprintf('Usage of non existing directory "%s" in the configuration "root_dir" of "cellulr_engine".', $config['root_dir']));
+            throw new \Exception(sprintf('Usage of non existing directory "%s" in the configuration "root_dir" of "cellulr_engine".', $this->config['root_dir']));
         }
 
         if (false === realpath($rootDir.'/Cell')) {
@@ -52,6 +54,10 @@ class EngineExtension extends Extension implements PrependExtensionInterface
 
         // Pre-set the DunglasActionBundle
         $this->prependAction($container, $directories);
+
+        $container->setParameter('cellulr_engine.default_rendering', $this->config['default_rendering']);
+        $container->setParameter('cellulr_engine.fail_loud', $this->config['serverside_rendering']['fail_loud']);
+        $container->setParameter('cellulr_engine.trace', $this->config['serverside_rendering']['trace']);
     }
 
     /**
@@ -61,6 +67,30 @@ class EngineExtension extends Extension implements PrependExtensionInterface
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $serverSideEnabled = $this->config['default_rendering'];
+
+        if (in_array($serverSideEnabled, array('both', 'server_side'), true)) {
+            $serverSideMode = $this->config['serverside_rendering']['mode'];
+            if ($serverSideMode === 'external_server') {
+                if ($serverSocketPath = $this->config['serverside_rendering']['server_socket_path']) {
+                    $container
+                        ->getDefinition('rf.cellulr.engine.external_react_renderer')
+                        ->addMethodCall('setServerSocketPath', array($serverSocketPath))
+                    ;
+                }
+                $renderer = $container->getDefinition('rf.cellulr.engine.external_react_renderer');
+            } else {
+                if ($serverBundlePath = $this->config['serverside_rendering']['server_bundle_path']) {
+                    $container
+                        ->getDefinition('rf.cellulr.engine.phpexecjs_react_renderer')
+                        ->addMethodCall('setServerBundlePath', array($serverBundlePath))
+                    ;
+                }
+
+                $renderer = $container->getDefinition('rf.cellulr.engine.phpexecjs_react_renderer');
+            }
+            $container->setDefinition('rf.cellulr.engine.react_renderer', $renderer);
+        }
     }
 
     /**
